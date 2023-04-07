c9c1b4a2d71cce2a3cb72d0a8e47f0ec6cf64b67
fenroe <scrambles9494@gmail.com>
2023-04-07 12:32:18 +0300

    finished users controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -33,6 +33,11 @@ export class UsersController {
     return await this.usersService.update(+id, updateUserDto);
   }
 
+  @Put('/credentials/:id')
+  async updateCredentials(@Param('id') id: string, @Body() updateCredentialDto: UpdateCredentialDto) {
+    return await this.usersService.updateCredentials(+id, updateCredentialDto);
+  }
+
   @Delete(':id')
   async remove(@Param('id') id: string) {
     return await this.usersService.remove(+id);
